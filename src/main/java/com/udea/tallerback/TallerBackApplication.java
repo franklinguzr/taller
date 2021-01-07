4c909f8c43e32b213083e23254085356eb0d73bf
FranklinGU <franklinguzr@gmail.com>
2021-01-07 18:54:46 -0500

    inicializacion proyecto

---
+++ src/main/java/com/udea/tallerback/TallerBackApplication.java
@@ -0,0 +1,13 @@
+package com.udea.tallerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TallerBackApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/udea/tallerback/exception/DataNotFoundException.java
@@ -0,0 +1,13 @@
+package com.udea.tallerback.exception;
+
+/**
+ * Exception for duplicated data in the application
+ */
+public class DataNotFoundException extends GeneralRuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
